571d7edf82dcfbc69c2a95e43bbad28382687eeb
Thilak Rao <me@thilak.in>
2018-04-02 19:03:28 +0200

    Created a simplistic page using Semantic UI React

--- react/src/App.js
+++ react/src/App.js
@@ -1,21 +1,105 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Grid, Input, Button, Segment } from 'semantic-ui-react';
 import './App.css';
+import isURL from 'is-url';
+
+const theme = {
+  error: "#9f3a38"
+};
+
+const globalStyle = {
+  margin: '14px'
+};
+
+const css = {
+  input: {
+    width: '100%'
+  },
+  inputContainer: {
+    marginTop: '50px'
+  },
+  buttonMargin: {
+    marginTop: globalStyle.margin
+  },
+  inputError: {
+    color: theme.error
+  }
+};
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: '',
+      validationError: false,
+      showLoading: false
+    }
+  }
+
+  showLoading() {
+    this.setState({
+      showLoading: true,
+    });
+  }
+
+  hideLoading() {
+    this.setState({
+      showLoading: false,
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.showLoading();
+    if (isURL(this.state.url)) {
+      this.setState({
+        validationError: false,
+      });
+
+    } else {
+      this.setState({
+        validationError: true,
+      });
+    }
+    this.hideLoading();
+  }
+
+  updateUrl(e) {
+    this.setState({
+      url: `http://${e.target.value}`
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Grid columns={2} verticalAlign={'middle'} container={true} stackable={true} centered>
+        <Grid.Row>
+          <Grid.Column>
+            <Segment stacked style={css.inputContainer}>
+              <form onSubmit={(e) => this.handleSubmit(e)}>
+                <label htmlFor="urlInput">
+                  Please type a URL
+                  <Input label='http://' name={"urlInput"} placeholder='Enter URL' style={css.input} focus
+                         onChange={(e) => this.updateUrl(e)} error={this.state.validationError}
+                         loading={this.state.showLoading} />
+                </label>
+                {this.state.validationError && <InputError></InputError>}
+                <Button content='Visualize' primary style={css.buttonMargin} type={"submit"}/>
+              </form>
+            </Segment>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     );
   }
+}
+
+function InputError(){
+  return (
+    <div style={css.inputError}>
+      <p>Please type a valid <a href="https://en.wikipedia.org/wiki/Hostname">hostname</a></p>
+    </div>
+  );
 }
 
 export default App;
--- react/src/index.js
+++ react/src/index.js
@@ -1,5 +1,7 @@
+import 'whatwg-fetch';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
